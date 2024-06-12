34a6437fbaefd6674fae46129b827e6dd7c3df68
svyatoslavkachmar-cnic <Svyatoslav.Kachmar@centralnic.com>
2024-06-12 14:54:21 +0300

    add editting capabilities

--- readme.md
+++ readme.md
@@ -1,37 +1,3 @@
-# In The Know Desktop
+To create an app you need to first run npm run pack and then nmp run dist and prbablt dist folder must be empty beforehand.
 
-Electron application that allows you to select an image and easily change the width and/or height.
-
-<div style="display: flex; justify-content: center">
-</div>
-
-## Usage
-
-Install dependencies:
-
-```bash
-
-npm install
-```
-
-Run:
-
-```bash
-npm start
-```
-
-You can also use `Electronmon` to constantly run and not have to reload after making changes
-
-```bash
 npx electronmon .
-```
-
-## Packaging
-
-There are multiple ways to package Electron apps. I would suggest [Electron Forge](https://www.electronforge.io/). I did not implement any packaging into this app.
-
-## Developer Mode
-
-If your `NODE_ENV` is set to `development` then you will have the dev tools enabled and available in the menu bar. It will also open them by default.
-
-When set to `production`, the dev tools will not be available.
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,90 +1,67 @@
 const screen = document.querySelector('#screen');
 const question = document.querySelector('#question');
+const questionInput = document.querySelector('#questionInput');
+const answer = document.querySelector('#answer');
+const answerInput = document.querySelector('#answerInput');
 const go = document.querySelector('#go');
 const showAnswer = document.querySelector('#showAnswer');
-const answer = document.querySelector('#answer');
 const number = document.querySelector('#number');
 const repeated = document.querySelector('#repeated');
+const save = document.querySelector('#save');
 
 let filteredQuestions = [],
   filteredIds = [],
   filteredRepeated = [],
   media = [],
   currentIndex = 0,
-  temporaryValue,
-  randomIndex,
   numberOfCurrentQuestion = 1;
 
-  async function fetchMedia() {
-    try {
-      // Make a GET request to the specified URL
-      const response = await fetch('https://in-the-know.blobsandtrees.online/wp-json/wp/v2/media');
-      
-      // Check if the response is successful (status code 200-299)
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      
-      // Parse the JSON response body
-      const data = await response.json();
-  
-      media = shuffleArray(data.map((item) => ({ uri: item.link })));
-
-      screen.style.backgroundImage = "url(" + media[0].uri + ")";
-    
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+async function fetchMedia() {
+  try {
+    const response = await fetch('https://in-the-know.blobsandtrees.online/wp-json/wp/v2/media');
+    if (!response.ok) throw new Error('Failed to fetch data');
+    const data = await response.json();
+    media = shuffleArray(data.map(item => ({ uri: item.link })));
+    screen.style.backgroundImage = `url(${media[0].uri})`;
+  } catch (error) {
+    console.error('Error fetching media:', error);
   }
-
-  async function updateRepeated(postId, newRepeated) {
-    const url = `https://in-the-know.blobsandtrees.online/wp-json/custom/v1/update-repeated/${postId}`;
-
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ new_repeated: newRepeated })
-        });
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-
-        const data = await response.json();
-        console.log('Success:', data);
-    } catch (error) {
-        console.error('Error:', error);
-    }
 }
-  
+
+async function updateRepeated(postId, newRepeated) {
+  try {
+    const response = await fetch(`https://in-the-know.blobsandtrees.online/wp-json/custom/v1/update-repeated/${postId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ new_repeated: newRepeated })
+    });
+    if (!response.ok) throw new Error('Network response was not ok ' + response.statusText);
+    const data = await response.json();
+    console.log('Success:', data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
 async function fetchData() {
   try {
     const response = await fetch('https://in-the-know.blobsandtrees.online/wp-json/custom/v1/question-posts');
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
+    if (!response.ok) throw new Error('Failed to fetch data');
     const data = await response.json();
 
-    // Find the minimum and maximum repeated numbers in the data array
     const minRepeat = Math.min(...data.map(question => question.repeated || Infinity));
     const maxRepeat = Math.max(...data.map(question => question.repeated || 0));
-
-    // Define a step size for creating the threshold
     const step = 1;
 
-    // Generate an array of thresholds from minRepeat to maxRepeat with the specified step size
     const thresholds = [];
     for (let i = minRepeat; i <= maxRepeat; i += step) {
       thresholds.push(i);
     }
 
-    // Shuffle the data array
     const shuffledData = shuffleArray(data.filter((item) => item?.question));
 
-    // Separate the shuffled data array into multiple arrays based on the thresholds
     const shuffledGroups = [];
     for (let i = 0; i < thresholds.length; i++) {
       const threshold = thresholds[i];
@@ -92,18 +69,16 @@ async function fetchData() {
       shuffledGroups.push(group);
     }
 
-    // Shuffle each group independently
     const shuffledGroupsShuffled = shuffledGroups.map(group => (group));
 
-    // Concatenate the shuffled groups to create the final shuffled data array
     const shuffledDataFinal = [].concat(...shuffledGroupsShuffled);
 
-    // Extract filteredQuestions, filteredIds, and filteredRepeateds from the shuffled array
     filteredQuestions = shuffledDataFinal;
     filteredIds = filteredQuestions.map(item => item.id);
-    filteredRepeateds = filteredQuestions.map(item => item.repeated);
+    filteredRepeated = filteredQuestions.map(item => item.repeated);
 
     question.textContent = filteredQuestions[currentIndex]?.question;
+    questionInput.textContent = filteredQuestions[currentIndex]?.question;
     number.textContent = numberOfCurrentQuestion + " / " + filteredQuestions.length;
 
   } catch (error) {
@@ -111,7 +86,6 @@ async function fetchData() {
   }
 }
 
-// Shuffle function
 const shuffleArray = (array) => {
   let currentIndex = array.length;
   let temporaryValue, randomIndex;
@@ -128,10 +102,7 @@ const shuffleArray = (array) => {
   return array;
 };
 
-// Call the fetchData function to initiate the request
 fetchData();
-
-// Call the fetchData function to initiate the request
 fetchMedia();
 
 const handleGoPress = () => {
@@ -140,43 +111,93 @@ const handleGoPress = () => {
   numberOfCurrentQuestion++;
 
   question.textContent = filteredQuestions[currentIndex]?.question;
+  questionInput.textContent = filteredQuestions[currentIndex]?.question;
 
   answer.style.display = "none";
-
   go.style.display = "none";
-
   showAnswer.style.display = "block";
-
-  screen.style.backgroundImage = "url(" + media[randomIndex].uri + ")";
-
+  screen.style.backgroundImage = `url(${media[randomIndex].uri})`;
   number.textContent = numberOfCurrentQuestion + " / " + filteredQuestions.length;
+  questionInput.style.display = "none";
+  answerInput.style.display = "none";
 
   if (numberOfCurrentQuestion !== filteredQuestions.length) {
-      return;
+    return;
   } else {
-      numberOfCurrentQuestion = 1;
-      currentIndex = 0;
+    numberOfCurrentQuestion = 1;
+    currentIndex = 0;
   }
 };
 
 const handleSeeAnswerPress = () => {
   answer.textContent = filteredQuestions[currentIndex]?.answer;
+  answerInput.textContent = filteredQuestions[currentIndex]?.answer;
 
-  filteredQuestions[currentIndex].featured_media ? screen.style.backgroundImage = "url(" + filteredQuestions[currentIndex].featured_media + ")" : null;
-
+  filteredQuestions[currentIndex].featured_media ? screen.style.backgroundImage = `url(${filteredQuestions[currentIndex].featured_media})` : null;
   showAnswer.style.display = "none";
+  answer.style.display = "block";
+  go.style.display = "block";
+  repeated.textContent = filteredQuestions[currentIndex]?.repeated;
+  updateRepeated(filteredIds[currentIndex], (Number(filteredQuestions[currentIndex].repeated) + 1));
+  questionInput.style.display = "none";
+  answerInput.style.display = "none";
 
   answer.style.display = "block";
+  question.style.display = "block";
+};
 
-  go.style.display = "block";
+const handleDisplayQuestionInput = () => {
+  questionInput.style.display = "block";
+  question.style.display = "none";
+  save.style.display = "block";
+};
 
-  repeated.textContent = filteredQuestions[currentIndex]?.repeated;
+const handleDisplayAnswerInput = () => {
+  answerInput.style.display = "block";
+  answer.style.display = "none";
+  save.style.display = "block";
+};
 
-  updateRepeated(filteredIds[currentIndex], (Number(filteredQuestions[currentIndex].repeated) + 1));
+const handleSavePress = async () => {
+  const updatedQuestion = questionInput.textContent;
+  const updatedAnswer = answerInput.textContent;
 
+  try {
+    const response = await fetch(`https://in-the-know.blobsandtrees.online/wp-json/custom/v1/update-question/${filteredIds[currentIndex]}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        question: updatedQuestion,
+        answer: updatedAnswer
+      })
+    });
+    console.log(response);
+    if (!response.ok) throw new Error('Network response was not ok ' + response.statusText);
+    const data = await response.json();
+    console.log('Success:', data);
+
+    // filteredQuestions[currentIndex].question = updatedQuestion;
+    // filteredQuestions[currentIndex].answer = updatedAnswer;
+
+    question.textContent = updatedQuestion;
+    answer.textContent = updatedAnswer;
+    save.style.display = "none";
+    if (answerInput.style.display === "block") {
+      answer.style.display = "block";
+    }
+    questionInput.style.display = "none";
+    answerInput.style.display = "none";
+    question.style.display = "block";
+  
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
 
 go.addEventListener("click", handleGoPress);
-
 showAnswer.addEventListener("click", handleSeeAnswerPress);
-
+save.addEventListener("click", handleSavePress);
+question.addEventListener("click", handleDisplayQuestionInput);
+answer.addEventListener("click", handleDisplayAnswerInput);
